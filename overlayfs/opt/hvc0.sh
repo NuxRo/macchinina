c614d1dddb74525225062d50b1d62e492596d498
root <root@builder.openvm.eu>
2014-12-20 00:46:32 +0000

    small fixes

--- overlayfs/opt/hvc0.sh
+++ overlayfs/opt/hvc0.sh
@@ -2,4 +2,4 @@
 
 # start a getty on hvc0, but only if we asked for it - ie this is called by /boot/grub/grub.conf when booted by pvgrub
 
-grep -q hvc0 /proc/cmdline && /sbin/getty -L  hvc0 || exit 0
+grep -q hvc0 /proc/cmdline && /sbin/getty -L  hvc0 115200 vt100 || sleep 86400
